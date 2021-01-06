8524270cf165124466d96e1b1375e32a202e879a
Denis Zhigun <zhigundv@gmail.com>
2021-01-06 21:40:37 +0300

    Added java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 1000;
+        int reward = 20;
+        int mile = cost / reward;
+        System.out.println(mile);
+
+        }
+
+    }
+
